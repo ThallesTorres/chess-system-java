2ab4e369d1f119613f2b2529d18af494906abd43
thallestorres <thalles.torres@pm.me>
2022-10-25 08:46:22 -0300

    First class: Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position pos = new Position(2, 4);
+        System.out.println(pos);
     }
 }
